c52d704c7be4194923e11182f3f61f9cd5939a58
VarakutiNikhitha <nikhithavarakuti@gmail.com>
2025-11-15 12:19:01 +0530

    Add files via upload

---
+++ NikyaFuraApplication.java
@@ -0,0 +1,13 @@
+package com.nikhitha.nikyafura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NikyaFuraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NikyaFuraApplication.class, args);
+	}
+
+}
